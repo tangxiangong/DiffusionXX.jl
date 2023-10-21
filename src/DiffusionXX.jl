a878c17f115a2c4b13885cb5f2149b515da975c8
小雨 <84885148+tangxiangong@users.noreply.github.com>
2023-10-21 11:58:15 +0800

    update

--- src/DiffusionXX.jl
+++ src/DiffusionXX.jl
@@ -1,4 +1,5 @@
 module DiffusionXX
+include("Random/Random.jl")
 include("discrete/DiscreteModel.jl")
-
+include("continuous/CTRW.jl")
 end # module DiffusionXX
--- src/visualization.jl
+++
@@ -1,7 +0,0 @@
-using Plots()
-pgfplotsx()
-
-function plot(model::RandomWalk1D)
-    x = simulate(model::RandomWalk1D)
-    plot(x, markershape=:circle, seriestype=:steppre, framestyle=:origin, draw_arrow=true)
-end
